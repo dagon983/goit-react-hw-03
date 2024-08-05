f9ccca757c19f649da5643105ff2e1748a386bb5
dagon983 <56299355+dagon983@users.noreply.github.com>
2024-08-05 23:33:10 +0300

    v.1.5

--- src/components/ContactList/Contact/Contact.jsx
+++ src/components/ContactList/Contact/Contact.jsx
@@ -7,17 +7,16 @@ import s from "./Contact.module.css"
 const Contact = ({contacts, deleteContact}) => {
   return (
     <div className={s.wrapper}>
-        {
-            contacts.map(item => (
-                <li className={s.li} key={item.id}>
-                    <p><FaUser className={s.icon}/>
-                    {item.name}</p>
-                    <p><FaPhone className={s.icon}/>
-                    {item.number}</p>
-                    <button className={s.deleteButton} type="button" onClick={() => deleteContact(item.id)}>Delete</button>
-                </li>
-            ))
-        }
+        
+        <li className={s.li}>
+            <p><FaUser className={s.icon}/>
+            {contacts.name}</p>
+            <p><FaPhone className={s.icon}/>
+            {contacts.number}</p>
+            <button className={s.deleteButton} type="button" onClick={() => deleteContact(contacts.id)}>Delete</button>
+        </li>
+           
+        
     </div>
   )
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,9 +1,15 @@
-import Contact from "./Contact/Contact"
+import Contact from "./Contact/Contact";
+import s from "./ContactList.module.css"
 
 const ContactList = ({ contacts, deleteContact }) => {
   return (
-    <ul>
-    <Contact contacts={contacts} deleteContact={deleteContact}/>
+    <ul className={s.ContactList}>
+        {
+            contacts.map((contacts) => {
+             return  <Contact contacts={contacts} deleteContact={deleteContact} key={contacts.id}/> 
+            })
+        }
+    
     </ul>
   )
 }
